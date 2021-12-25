6bd2e4f6001e5cd6ab05024cd7504b344d8b540e
hahahahz1 <newdisc123@protonmail.com>
2021-12-25 11:04:19 -0500

    dd

--- Modules/Features/AntiShield.lua
+++ Modules/Features/AntiShield.lua
@@ -1,1 +1,8 @@
-local hi = ""
+local oldIndex
+oldIndex = hookmetamethod(game, "__index", function(i, v) -- anti shield
+    if (tostring(i) == "Blocking" and v == "Value" and getgenv().settings.combat.antiShield.enabled and math.random(1, 100) <= getgenv().settings.combat.antiShield.chance) then
+        return false 
+    end
+
+    return oldIndex(i, v)
+end)
